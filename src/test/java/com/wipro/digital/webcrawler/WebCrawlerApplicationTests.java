1df76e5b54ff8b609752bfbca7e53a1df06d1e25
baira8ms <baira8ms@gmail.com>
2020-11-09 22:02:04 +0000

    Added integration test to verify the commandline runner invocation

--- src/test/java/com/wipro/digital/webcrawler/WebCrawlerApplicationTests.java
+++ src/test/java/com/wipro/digital/webcrawler/WebCrawlerApplicationTests.java
@@ -1,14 +1,23 @@
 package com.wipro.digital.webcrawler;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import com.wipro.digital.webcrawler.service.WebCrawlerExecutorService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 
 @SpringBootTest
 class WebCrawlerApplicationTests {
 
-	@Test
-	void executeWebCrawler() {
+  @SpyBean
+  WebCrawlerExecutorService webCrawlerExecutorService;
 
-	}
+  @Test
+  void whenContextLoads_thenRunnersRun() throws Exception {
+    verify(webCrawlerExecutorService, times(1)).run(any());
+  }
 
 }
